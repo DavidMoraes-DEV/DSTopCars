c11499c43c8dcf7ca1129ef62cec54a1d49abf02
DavidMoraes-DEV <investtraderrs@gmail.com>
2022-08-19 19:37:02 -0300

    Implementando e Estilizando a Home Page

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const Button = () => {
+    return (
+            <button className='btn btn-primary button-container'>
+                <h6>VER CATÁLOGO</h6>
+            </button>
+    );
+}
+
+export default Button;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,30 @@
+import './styles.css';
+import 'App.css';
+import MainImage from 'assets/images/car-card.png';
+import Button from 'components/Button';
 
 const Home = () => {
     return (
-        <h1>HOME PAGE</h1>
-    );
+        <div className='home-container'>
+            <div className='bg-primary global-container home-card'>
+                <div className='home-content-container'>
+                    <h1>
+                        O CARRO PERFEITO PARA VOCÊ
+                    </h1>
+                    <p>
+                        Conheça nossos carros e dê mais um passo na realização do seu sonho
+                    </p>
+                </div>
+                <div className='home-image-container'>
+                    <img className='image-card' src={MainImage} alt="Main-Card" />
+                </div>
+            </div>
+            <div className='bg-secodary home-card-button'>
+                <p>Comece agora a navegar</p>
+                <Button />
+            </div>
+        </div>
+    )
 }
 
 export default Home;
